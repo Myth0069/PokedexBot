87076608fe7115677c596c68762a7b5eae27cc1f
AndrewWritesCode <47736760+AndrewWritesCode@users.noreply.github.com>
2021-01-01 00:07:56 -0800

    Modifyed JSON generator to add gen and pkmn no to JSON

--- JSON Generation/JSONgenerator.py
+++ JSON Generation/JSONgenerator.py
@@ -2,6 +2,7 @@ import json
 
 
 pokemonNames = open('pokemonALPH.txt', 'r')
+pokemonNumbers = open('pokemonByNumber.txt', 'r')
 line = 0
 
 BurlB = r'https://bulbapedia.bulbagarden.net/wiki/'
@@ -23,7 +24,7 @@ for line in pokemonNames:
     count += 1
     name = str(line.strip())
     tempname = name
-    score = wordScore(tempname)
+   
     name = {
         "name": tempname,
         "bulbapedia": BurlB + tempname + BurlE,
@@ -33,6 +34,53 @@ for line in pokemonNames:
         }
     pokemon[tempname] = name
 pokemonNames.close()
+
+for entry in pokemonNumbers:
+    entryLine = str(entry.strip())
+    digitcount = 0
+    gen = 8
+    no = 0
+    write = False
+    number = ''
+    candidate = ''
+    for character in entryLine:
+        if character == '#':
+            write = True
+        if (write == True) and (digitcount < 4):
+           number += character
+           digitcount += 1                
+        elif write == True:
+            candidate = candidate + character
+    candidate = candidate.strip()
+    candidate = candidate.lower()
+    number = number[1:]
+    no = int(number)
+ 
+    if no < 152:
+        gen = 1
+    elif no < 252:
+        gen = 2
+    elif no < 387:
+        gen =3
+    elif no < 494:
+        gen = 4
+    elif no < 650:
+        gen = 5
+    elif no < 722:
+        gen = 6
+    elif no < 810:
+        gen = 7
+    elif no == 0:
+        gen = 8
+
+    try:
+        dict = pokemon[candidate]
+        dict["no"] = no
+        dict["gen"] = gen
+    except:
+        pass
+
+pokemonNumbers.close()
 
 json_object = json.dumps(pokemon, indent = 4)
 f = open("pokemonALPH.json", "w")
